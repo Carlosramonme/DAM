4033b0ce969c09abd7fa38307f3b54af7c02d78d
Carlosramonme <32344724+Carlosramonme@users.noreply.github.com>
2017-09-28 02:29:48 +0200

    Codigo nocturno.

--- Retratos Robot/src/RetratosRobot.java
+++ Retratos Robot/src/RetratosRobot.java
@@ -1,9 +1,61 @@
+import java.util.Scanner;
+
+import obj.retratos.Retratos;
 
 public class RetratosRobot {
+	
+	Scanner teclado = new Scanner(System.in);
+	
+	private int ojos;
+	private int pelo;
+	private int nariz;
+	private int boca;
+	
+	public void pregunta(){
+		
+		System.out.println("Dime un tipo de Pelo 1-4");
+		
+		ojos = teclado.nextInt();
+		
+		System.out.println("Dime un tipo de Ojos 1-4");
+		
+		pelo = teclado.nextInt();
+		
+		System.out.println("Dime un tipo de nariz 1-4");
+		
+		nariz = teclado.nextInt();
+		
+		System.out.println("Dime un tipo de boca 1-4");
+	
+		boca = teclado.nextInt();
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		RetratosRobot respuesta = new RetratosRobot();
+		
+		respuesta.pregunta();
+		 
+		 
+		 
+		 Retratos retrato;
+		 
+		 Retratos cara = new Retratos();
+		 
+		 cara.imprimir(respuesta.ojos, respuesta.pelo, respuesta.nariz, respuesta.boca);
+		
+		
+	
+	
 	}
+
+
+
+
+
+
+	
+
+	
 
 }
--- Retratos Robot/src/obj/retratos/Retratos.java
+++ Retratos Robot/src/obj/retratos/Retratos.java
@@ -2,4 +2,88 @@ package obj.retratos;
 
 public class Retratos {
 
+//	private String ojos;
+//	private String pelo;
+//	private String nariz;
+//	private String boca;
+	
+	
+	public void imprimir(int ojos, int pelo, int nariz, int boca) {
+
+	switch (pelo) {
+	case 1:
+		System.out.println("WWWWWWWWW");
+		break;	
+	case 2:
+		System.out.println("\\\\//////");
+		break;
+	case 3:
+		System.out.println("|\"\"\"\"\"\"\"|");
+		break;
+	case 4:
+		System.out.println("|||||||||");
+		break;
+
+	default:
+		break;
+	}
+	
+	switch (ojos) {
+	case 1:
+		System.out.println("|  O O  |");
+		break;	
+	case 2:
+		System.out.println("|- (·  ·) -|");
+		break;
+	case 3:
+		System.out.println("|- (o  o) -|");
+		break;
+	case 4:
+		System.out.println("|  \\  /  |");
+		break;
+
+	default:
+		break;
+	}
+	
+	switch (nariz) {
+	case 1:
+		System.out.println("@   J   @");
+		break;	
+	case 2:
+		System.out.println("{  \"  }");
+		break;
+	case 3:
+		System.out.println("[   j   ]");
+		break;
+	case 4:
+		System.out.println("<  -  >");
+		break;
+
+	default:
+		break;
+	}
+	
+	switch (boca) {
+	case 1:
+		System.out.println("|  ===  |");
+		break;	
+	case 2:
+		System.out.println("|  -  |");
+		break;
+	case 3:
+		System.out.println("|  ___  |");
+		break;
+	case 4:
+		System.out.println("|  ---  |");
+		break;
+
+	default:
+		break;
+	}
+	
+	System.out.println(" \\_____/");
+		
+	}
+	
 }
