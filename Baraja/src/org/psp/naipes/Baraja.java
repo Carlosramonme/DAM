3df8d5835ca6deb4dd4b7127ae8ad1f76a68e4d9
Carlosramonme <32344724+Carlosramonme@users.noreply.github.com>
2017-10-04 14:33:36 +0200

    Ñe

--- Baraja/src/org/psp/naipes/Baraja.java
+++ Baraja/src/org/psp/naipes/Baraja.java
@@ -5,10 +5,39 @@ import org.psp.naipes.Naipe.rango;
 
 public class Baraja {
 
-	private palo[] palos= palo.values();
-	private rango[] rangos = rango.values();	
 	
-
-
+	static String miBaraja [][];
+	
+	public static void crearBaraja()  {
+	
+		for (palo p : palo.values()) { //4
+			
+			miBaraja[p.ordinal()][0] = palo.valueOf(p);
+			
+			for (rango r : rango.values()){ //14
+				
+				miBaraja[p.ordinal()][r.ordinal()]= p.toString();
+				
+			}
+			
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+			
+	crearBaraja();
+		
+		for (palo p : palo.values()) { //4
+			
+			
+			for (rango r : rango.values()){ //14
+				
+				System.out.println(miBaraja[p.ordinal()][r.ordinal()]);
+				
+			}
+			
+		}
+}
 	
 }
